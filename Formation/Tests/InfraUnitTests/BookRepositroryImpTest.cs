9a79c35bbaf86660483b1395694c6e625b711d6a
Lucas Gauvain <lucasgauvain@gmail.com>
2023-06-01 12:16:08 +0200

    added getallbooks test

--- Formation/Tests/InfraUnitTests/BookRepositroryImpTest.cs
+++ Formation/Tests/InfraUnitTests/BookRepositroryImpTest.cs
@@ -49,5 +49,42 @@ namespace InfraUnitTests
             Assert.True(result > 0);
             _dbContext.Books.Count().Should().Be(1);
         }
+
+          [Fact]
+        public async Task ShouldGetAllBooks()
+        {
+            var author = new Author
+            {
+                FirstName = "Test",
+                LastName = "Test",
+                BirthDate = DateTime.Now
+            };
+
+            _dbContext.Authors.Add(author);
+            await _dbContext.SaveChangesAsync();
+
+            var book1 = new BookDto()
+            {
+                Title = "Test1",
+                AuthorId = 1,
+                Description = "Test1",
+                PublicationDate = DateTime.Now
+            };
+
+            var book2 = new BookDto()
+            {
+                Title = "Test2",
+                AuthorId = 1,
+                Description = "Test2",
+                PublicationDate = DateTime.Now
+            };
+
+            await _repository.AddBook(book1);
+            await _repository.AddBook(book2);
+
+            var books = await _repository.GetAllBooks();
+
+            books.Count().Should().Be(2);
+        }
     }
 }
